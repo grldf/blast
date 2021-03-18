58ca300e6871ffee3496abbb4f969edc8ca4dead
faure <grldfaure@gmail.com>
2021-03-18 14:13:18 +0100

    test pas à pas 1

---
+++ src/routes/index.svelte
@@ -0,0 +1,36 @@
+<div class="video">
+	<video autoplay muted loop src="vidtext.mp4" type="video/mp4"></video>
+	<img src="logo-512.png" alt="logo deco" />
+</div>
+<style>
+	.video{
+		width:100%;
+		height:100vh;
+	}
+	.video video{
+		position:absolute;
+		top:0;
+		left:0;
+		width:100%;
+		height:100%;
+		object-fit:cover;
+	}
+	
+	.video img{
+		position:absolute;
+		top:0;
+		left:0;
+		width:100%;
+		height:100%;
+		margin:0;
+		padding:0;
+		font-size:30vw;
+		display:flex;
+		justify-content:center;
+		align-items:center;
+		text-align:center;
+		color:#000;
+		background:#fff;
+		mix-blend-mode: screen;
+	}
+</style>
